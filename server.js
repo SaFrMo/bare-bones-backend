f6210e758acb4859d9e427f52fb90067096840b9
Sander <sander@funkhaus.us>
2017-01-27 11:02:27 -0500

    Ignoring npm debug and starting basic API functionality

--- server.js
+++ server.js
@@ -1,20 +1,48 @@
 import path from 'path'
 import bookshelf from 'services/bookshelf'
 import _ from 'lodash'
+import express from 'express'
 
 global.appRoot = path.resolve(__dirname)
 
-import Book from 'models/book'
+let app                 = express()
+let port                = process.env.PORT || 3000
+let router              = express.Router()
 
-var nextBook = new Book({
-    title: 'Juneteenth',
-    author_id: 1
-})
-nextBook.save()
+// Import routes
+import addRoute         from 'routes/add'
+import listRoute        from 'routes/list'
 
-Book.forge().fetchAll({ withRelated: 'author' }).then(collection => {
-    //console.log(author.related('author').get('name'))
-    _.forEach( collection.serialize(), val => {
-        console.log(`${val.title}, by ${val.author.name}\n`)
+// heartbeat
+router.get('/', (req, res) => {
+    res.json({
+        meta: {
+            success: true,
+            code: 200,
+            message: 'The API is up and running.'
+        }
     })
 })
+
+// Map routes
+//router.use('/add',      addRoute)
+//router.use('/list',     listRoute)
+
+// Start service (prefixed with version)
+app.use('/v1', router)
+app.listen(port)
+console.log(`Running on port ${port} at endpoint /v1/`)
+
+// import Book from 'models/book'
+//
+// var nextBook = new Book({
+//     title: 'Juneteenth',
+//     author_id: 1
+// })
+// nextBook.save()
+//
+// Book.forge().fetchAll({ withRelated: 'author' }).then(collection => {
+//     _.forEach( collection.serialize(), val => {
+//         console.log(`${val.title}, by ${val.author.name}\n`)
+//     })
+// })
