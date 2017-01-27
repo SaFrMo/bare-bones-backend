4018d5a76c44af87013f370099b82183c25ba2b1
Sander <sander@funkhaus.us>
2017-01-27 10:48:59 -0500

    Example adding and saving a book

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 import path from 'path'
 import bookshelf from 'services/bookshelf'
+import _ from 'lodash'
 
 global.appRoot = path.resolve(__dirname)
 
 import Book from 'models/book'
-import Author from 'models/author'
 
-Book.forge().fetch({ withRelated: 'author' }).then(author => {
-    console.log(author.related('author').get('name'))
+var nextBook = new Book({
+    title: 'Juneteenth',
+    author_id: 1
+})
+nextBook.save()
+
+Book.forge().fetchAll({ withRelated: 'author' }).then(collection => {
+    //console.log(author.related('author').get('name'))
+    _.forEach( collection.serialize(), val => {
+        console.log(`${val.title}, by ${val.author.name}\n`)
+    })
 })
